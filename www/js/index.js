fee8c510d98c699318ae0f23d0044057c83faf90
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-03-30 18:24:14 -0500

    Discover and connect to new channels

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function _onLoad() {
 
+  // These are the characters you can easily type on Firefox OS
+  // without using the shift/alt key. The characters must be valid Firebase keys.
+  var chars = 'abcdefghijklmnopqrstuvwxyz_@'.split('');
+
   function Data() {
     this.conn = new Firebase('https://omnicast.firebaseio.com/');
     this.channels = this.conn.child('channels');
@@ -33,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function _onLoad() {
     }
     this.channel = this.channels.child(name);
     this.channel.on('child_added', function(snapshot) {
-      console.log('child_added ', snapshot.val());
+      console.log('child_added', snapshot.val());
       self.items.unshift(snapshot.val());
       self.refStack.unshift(snapshot.ref());
       self.cleanUpStack();
@@ -50,13 +54,16 @@ document.addEventListener('DOMContentLoaded', function _onLoad() {
   }
 
   var data = new Data();
-  ko.applyBindings(data, document.getElementById('links'));
+  ko.applyBindings(data, document.getElementById('data'));
 
 
   function Connect(data) {
     this.data = data;
     this.connected = ko.observable(false);
+    this.showJoinControls = ko.observable(false);
+    this.channelToJoin = ko.observable('')
     this.channelName = ko.observable('')
+    //window.localStorage.setItem('channel', '');
 
     var channel = window.localStorage.getItem('channel');
     if (channel) {
@@ -70,15 +77,40 @@ document.addEventListener('DOMContentLoaded', function _onLoad() {
     this.data.enabled(false);
   }
 
-  Connect.prototype.newChannel = function _newChannel() {
-    this.joinChannel('some-channel');
+  Connect.prototype.newChannel = function _newChannel(ctx, event) {
+    // TODO: make some timeout here and up the string length.
+    var self = this;
+    var name = '';
+    for (var i=0; i < 5; i++) {
+      name += chars[Math.floor(Math.random() * chars.length)];
+    }
+    console.log('trying to connect to', name);
+    this.data.channels.child(name).on('value', function _onValue(snapshot) {
+      if (!snapshot.val()) {
+        // We found an open channel; join it.
+        self.joinChannel(name);
+      } else {
+        setTimeout(self.newChannel.bind(self), 100);
+      }
+    });
   }
 
+  Connect.prototype.enterJoinControls = function _enterJoinControls() {
+    this.showJoinControls(true);
+  };
+
+  Connect.prototype.joinSelectedChannel = function _joinSelectedChannel() {
+    var name = this.channelToJoin();
+    this.channelToJoin('');
+    this.joinChannel(name);
+  };
+
   Connect.prototype.joinChannel = function _joinChannel(name) {
+    this.showJoinControls(false);
     this.connected(true);
     this.channelName(name);
     window.localStorage.setItem('channel', name);
-    this.data.joinChannel('some-channel');
+    this.data.joinChannel(name);
   };
 
   var connect = new Connect(data);
